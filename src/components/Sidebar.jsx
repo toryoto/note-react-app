5ef2836275c22ae88773904a7b93518cef6e61d6
toryoto <s1f102002345@iniad.org>
2024-06-27 02:47:08 +0900

    編集したのーとが上に来るようソートする

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import "./Sidebar.css"
 
 function Sidebar({ onAddNote, onDeleteNote,notes, activeNote, setActiveNote }) {
+  const sortedNotes = notes.sort((a, b) => b.modDate - a.modDate);
+
+
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
@@ -8,7 +11,7 @@ function Sidebar({ onAddNote, onDeleteNote,notes, activeNote, setActiveNote }) {
         <button onClick={onAddNote}>追加</button>
       </div>
       <div className="app-sidebar-notes">
-        {notes.map((note) => (
+        {sortedNotes.map((note) => (
           <div 
             className={`app-sidebar-note  ${note.id === activeNote && "active"}`}
             key={note.id}
